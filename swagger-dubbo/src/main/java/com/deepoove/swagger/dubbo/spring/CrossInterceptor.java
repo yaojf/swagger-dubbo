38a40961daa8ec3d908e1b2bdd35a25e9dd22fac
yaojiafeng <18784660@qq.com>
2019-03-14 17:35:13 +0800

    swagger静态页面添加到jar包里

--- swagger-dubbo/src/main/java/com/deepoove/swagger/dubbo/spring/CrossInterceptor.java
+++ swagger-dubbo/src/main/java/com/deepoove/swagger/dubbo/spring/CrossInterceptor.java
@@ -15,10 +15,7 @@ public class CrossInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         response.addHeader("Access-Control-Allow-Origin", "*");
-        response.addHeader("Access-Control-Allow-Methods", "*");
-        response.addHeader("Access-Control-Max-Age", "100");
-        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-        response.addHeader("Access-Control-Allow-Credentials", "false");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
         return super.preHandle(request, response, handler);
     }
 
